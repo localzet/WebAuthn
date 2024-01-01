45320e10df2cc34b040fa1bb772d63df978ecc8c
localzet <creator@localzet.com>
2024-01-01 19:33:57 +0300

    Happy New Year!

--- src/WebAuthn/Attestation/Format/Apple.php
+++ src/WebAuthn/Attestation/Format/Apple.php
@@ -5,7 +5,7 @@
  * @link        https://github.com/localzet/WebAuthn
  *
  * @author      Ivan Zorin <creator@localzet.com>
- * @copyright   Copyright (c) 2018-2023 Localzet Group
+ * @copyright   Copyright (c) 2018-2024 Localzet Group
  * @license     GNU Affero General Public License, version 3
  *
  *              This program is free software: you can redistribute it and/or modify
